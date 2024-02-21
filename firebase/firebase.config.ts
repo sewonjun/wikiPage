615426958d0235c91dc7436f246dd0f0a7f2b157
sewonjun <thornapple74@gmail.com>
2024-02-22 02:37:17 +0900

    set: firebase cloud storage 초기 설정

---
+++ firebase/firebase.config.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  type: "service_account",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  private_key_id: import.meta.env.VITE_FIREBASE_PRIVATE_KEY_ID,
+  private_key: import.meta.env.VITE_FIREBASE_PRIVATE_KEY,
+  client_email: import.meta.env.VITE_FIREBASE_EMAIL,
+  client_id: import.meta.env.VITE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-7tjr8%40wikipage-2a587.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { app, db };
