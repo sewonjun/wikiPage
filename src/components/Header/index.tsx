4ee91b3dd9d0fefd2d14bd313b61ac64f68ca0aa
sewonjun <thornapple74@gmail.com>
2024-02-24 00:25:50 +0900

    logo 설정

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/WikiPage.svg";
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="" width="200px" height={"50px"} />
+    </header>
+  );
+}
+
+export default Header;
