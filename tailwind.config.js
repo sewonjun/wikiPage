6e6465d1d801802967547ce53d64034877487720
sewonjun <thornapple74@gmail.com>
2024-02-23 17:51:12 +0900

    set: tailwindcss 설정

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+import plugin from "tailwindcss/plugin";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,html,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".btn": {
+          padding: ".5rem 1rem",
+          borderRadius: ".25rem",
+          fontWeight: "600",
+        },
+        ".btn-blue": {
+          backgroundColor: "#3490dc",
+          color: "#fff",
+          "&:hover": {
+            backgroundColor: "#2779bd",
+          },
+        },
+      });
+    }),
+  ],
+};
